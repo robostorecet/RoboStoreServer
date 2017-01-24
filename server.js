b90105b61ed64d7902cfbdc4cee4acbac0e18317
Shabeeb <shabeebrp@gmail.com>
2017-01-24 23:30:12 +0530

    Testing

--- server.js
+++ server.js
@@ -6,16 +6,10 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-//app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-//app.use(express.static(__dirname + '/public'));
-
 // set the home page route
 app.get('/', function(req, res) {
 
-    // ejs render automatically looks in the views folder
+    
     	MongoClient.connect(process.env.PROD_MONGODB, function (err, db) {
 	  if (err)
 	    console.log('Unable to connect to the mongoDB server. Error:', err);
@@ -26,7 +20,7 @@ app.get('/', function(req, res) {
       		if (err)
        		    console.log(err);
 		else
-			res.json(result[0]);
+			res.json(result);
 	    });
 	   db.close();
 	  }
